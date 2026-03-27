1ace396fadd8d0a5afb4ee95485eede5a18b1bca
imzinfsec <imzinfsec@gmail.com>
2026-03-27 11:39:19 +0900

    restart

---
+++ infra-basic-deploy-lab/infra-basic-deploy-lab/src/main/java/com/example/deploylab/ItemRepository.java
@@ -0,0 +1,5 @@
+package com.example.deploylab;
+
+public class ItemRepository {
+
+}
---
+++ infra-basic-deploy-lab/infra-basic-deploy-lab/src/main/java/com/example/deploylab/controller/BackendController.java
@@ -0,0 +1,48 @@
+// src/main/java/com/example/deploylab/BackendController.java
+// 20260327
+package com.example.deploylab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BackendController {
+	
+	@GetMapping("/healthcheck")
+	public String healthcheck() {
+		return "healthcheck";
+	}
+
+    @GetMapping("/backend")
+    public String backend() {
+        return "backend";
+    }
+    
+    @GetMapping("/backend2")
+    public String backend2() {
+        return "backend2";
+    }
+    
+    @GetMapping("/backend3")
+    public String backend3() {
+        return "backend3";
+    }
+    @GetMapping("/backend4")
+    public String backend4() {
+    	return "backend4";
+    }
+    @GetMapping("/backend5")
+    public String backend5() {
+    	return "backend5";
+    }
+    @GetMapping("/backend6")
+    public String backend6() {
+    	return "backend6";
+    }
+    @GetMapping("/backend77")
+    public String backend77() {
+    	return "backend77";
+    }
+}
